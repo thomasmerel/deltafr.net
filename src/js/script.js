d66321c6a3dbb706a16000a05846161b861d8861
Vulture <thomas.merel.44@gmail.com>
2018-05-13 18:11:19 +0200

    start

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - 103
+    }, 500);
+
+    $('body').scrollspy({
+        target: '#navbar'
+    });
+});
