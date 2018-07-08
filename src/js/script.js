371dffb4e9c553c2b78684b37470964ac59b1124
Vulture <thomas.merel.44@gmail.com>
2018-07-08 20:26:28 +0200

    add konami bomberman

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,22 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         target: '#navbar'
     });
 });
+
+var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+    n = 0;
+$(document).keydown(function (e) {
+    if (e.keyCode === k[n++]) {
+        if (n === k.length) {
+            $('#modal').modal('show');
+            n = 0;
+            return false;
+        }
+    }
+    else {
+        n = 0;
+    }
+});
+
+$('#modal').on('shown.bs.modal', function () {
+    gGameEngine.load()
+});
