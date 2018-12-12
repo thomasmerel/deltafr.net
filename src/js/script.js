f3b2040d4930d80cfa11e31a9b3483029e95d0f5
Vulture <thomas.merel.44@gmail.com>
2018-12-12 17:09:21 +0100

    add discord + copy + add notify + add calc age + add phoenix goat and mouse

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,93 @@
 var leaders = [
-    ['Eagle', 'src/img/deltas/eagle.jpg', '', 'https://steamcommunity.com/profiles/76561197970728039', ''],
-    ['Raptor', 'src/img/deltas/raptor.png', '', 'https://steamcommunity.com/profiles/76561197961915318', ''],
-    ['Warthog', 'src/img/deltas/warthog.jpg', '', 'https://steamcommunity.com/profiles/76561198066334393', ''],
+    ['Eagle', 'src/img/deltas/eagle.jpg', '1985-04-12', 'https://steamcommunity.com/profiles/76561197970728039', '', 'Eagle#2104'],
+    ['Raptor', 'src/img/deltas/raptor.png', '', 'https://steamcommunity.com/profiles/76561197961915318', '', 'DeltaFr_Raptor#0649'],
+    ['Warthog', 'src/img/deltas/warthog.jpg', '', 'https://steamcommunity.com/profiles/76561198066334393', '', '[DeltaFr]Warthog#9629']
 ];
 
 var members = [
-    ['Alcyon', 'src/img/deltas/alcyon.jpg', '', 'https://steamcommunity.com/profiles/76561198010847814', ''],
-    ['Ant', 'src/img/deltas/ant.jpg', '', 'https://steamcommunity.com/id/Tuponia', 'https://www.twitch.tv/deltafrant'],
-    ['Bear', 'src/img/deltas/bear.jpg', '25 ans', 'https://steamcommunity.com/profiles/76561198008978169', 'https://www.twitch.tv/thenossy'],
-    ['Blackbird', 'src/img/deltas/blackbird.jpg', '', 'https://steamcommunity.com/profiles/76561198053539965', ''],
-    ['Cheetah', 'src/img/deltas/cheetah.png', '', '', ''],
-    ['Coyote', 'src/img/deltas/coyote.png', '', '', ''],
-    ['Crow', 'src/img/deltas/crow.jpg', '', 'https://steamcommunity.com/profiles/76561198029587542', ''],
-    ['Dogg', 'src/img/deltas/dogg.jpg', '', 'https://steamcommunity.com/profiles/76561197985342238', ''],
-    ['Fennec', 'src/img/deltas/fennec.png', '21 ans', 'https://steamcommunity.com/profiles/76561198039833419/', 'https://www.twitch.tv/mr_lo0ky'],
-    ['Gecko', 'src/img/deltas/gecko.jpg', '', '', ''],
-    ['Gerfaut', 'src/img/deltas/gerfaut.jpg', '', 'https://steamcommunity.com/profiles/76561198011076482', ''],
-    ['Guanaco', 'src/img/deltas/guanaco.jpg', '', 'https://steamcommunity.com/profiles/76561198021452134', ''],
-    ['Gorilla', 'src/img/deltas/gorilla.png', '', 'https://steamcommunity.com/profiles/76561193787144601', ''],
-    ['Harfang', 'src/img/deltas/harfang.jpg', '', 'https://steamcommunity.com/profiles/76561198105484872', ''],
-    ['Ikran', 'src/img/deltas/ikran.png', '26 ans', 'https://steamcommunity.com/id/Ciiz79', ''],
-    ['Licorne', 'src/img/deltas/licorne.jpg', '', '', ''],
-    ['Octopus', 'src/img/deltas/octopus.jpg', '31 ans', 'https://steamcommunity.com/profiles/76561198016252308/', ''],
-    ['Panda', 'src/img/deltas/panda.png', '25 ans', 'https://steamcommunity.com/id/iEledys', ''],
-    ['Redbull', 'src/img/deltas/redbull.png', '', 'https://steamcommunity.com/id/deltafr_redbull', ''],
-    ['Scarabe', 'src/img/deltas/scarabe.png', '', 'https://steamcommunity.com/profiles/76561198030682725', ''],
-    ['Shark', 'src/img/deltas/shark.jpg', '', '', ''],
-    ['Stoat', 'src/img/deltas/stoat.png', '', '', ''],
-    ['Tiger', 'src/img/deltas/tiger.png', '', '', ''],
-    ['Vulture', 'src/img/deltas/vulture.png', '24 ans', 'https://steamcommunity.com/id/Vulture__/', 'https://www.twitch.tv/vulture____'],
-    ['Walrus', 'src/img/deltas/walrus.jpg', '', 'https://steamcommunity.com/profiles/76561198002184444', '']
+    ['Alcyon', 'src/img/deltas/alcyon.jpg', '', 'https://steamcommunity.com/profiles/76561198010847814', '', 'alcyon972#2705'],
+    ['Ant', 'src/img/deltas/ant.jpg', '', 'https://steamcommunity.com/id/Tuponia', 'https://www.twitch.tv/deltafrant', 'Braco\'#1055'],
+    ['Bear', 'src/img/deltas/bear.jpg', '1993-07-05', 'https://steamcommunity.com/profiles/76561198008978169', 'https://www.twitch.tv/thenossy', 'Bear / Cyniade#2680'],
+    ['Blackbird', 'src/img/deltas/blackbird.jpg', '', 'https://steamcommunity.com/profiles/76561198053539965', '', 'BlackBird#8088'],
+    ['Cheetah', 'src/img/deltas/cheetah.png', '', '', '', '[DeltaFr]Cheetah#7223'],
+    ['Coyote', 'src/img/deltas/coyote.png', '', '', '', 'Coyote#9216'],
+    ['Crow', 'src/img/deltas/crow.jpg', '', 'https://steamcommunity.com/profiles/76561198029587542', '', ''],
+    ['Dogg', 'src/img/deltas/dogg.jpg', '', 'https://steamcommunity.com/profiles/76561197985342238', '', 'Dogg#8105'],
+    ['Fennec', 'src/img/deltas/fennec.png', '1997-07-06', 'https://steamcommunity.com/profiles/76561198039833419/', 'https://www.twitch.tv/mr_lo0ky', 'Fennec#4338'],
+    ['Gecko', 'src/img/deltas/gecko.jpg', '', '', '', ''],
+    ['Gerfaut', 'src/img/deltas/gerfaut.jpg', '', 'https://steamcommunity.com/profiles/76561198011076482', '', 'Gerfaut#8330'],
+    ['Goat', 'src/img/deltas/avatar.jpg', '', '', '', 'Nativ_JuFa#1844'],
+    ['Gorilla', 'src/img/deltas/gorilla.png', '', 'https://steamcommunity.com/profiles/76561193787144601', '', 'Dr4g0v 🔞#9815'],
+    ['Guanaco', 'src/img/deltas/guanaco.jpg', '1986-02-27', 'https://steamcommunity.com/profiles/76561198021452134', '', 'Guanaco#5408'],
+    ['Harfang', 'src/img/deltas/harfang.jpg', '', 'https://steamcommunity.com/profiles/76561198105484872', '', 'Light#9645'],
+    ['Ikran', 'src/img/deltas/ikran.png', '1992-01-30', 'https://steamcommunity.com/id/Ciiz79', '', 'Ci\'iz#3573'],
+    ['Mouse', 'src/img/deltas/avatar.jpg', '', '', '', 'la sourie#6630'],
+    ['Licorne', 'src/img/deltas/licorne.jpg', '', '', '', ''],
+    ['Octopus', 'src/img/deltas/octopus.jpg', '1987-05-15', 'https://steamcommunity.com/profiles/76561198016252308/', '', 'Octopus#8323'],
+    ['Panda', 'src/img/deltas/panda.png', '1993-01-01', 'https://steamcommunity.com/id/iEledys', '', ''],
+    ['Phoenix', 'src/img/deltas/avatar.jpg', '', '', '', 'Devy#3943'],
+    ['Redbull', 'src/img/deltas/redbull.png', '', 'https://steamcommunity.com/id/deltafr_redbull', '', 'Red#1343'],
+    ['Scarabe', 'src/img/deltas/scarabe.png', '', 'https://steamcommunity.com/profiles/76561198030682725', '', 'Scarabe#6100'],
+    ['Shark', 'src/img/deltas/shark.jpg', '', '', '', ''],
+    ['Stoat', 'src/img/deltas/stoat.png', '', '', '', 'C0C0nutzz#9413'],
+    ['Tiger', 'src/img/deltas/tiger.png', '', '', '', ''],
+    ['Vulture', 'src/img/deltas/vulture.png', '1994-03-14', 'https://steamcommunity.com/id/Vulture__/', 'https://www.twitch.tv/vulture____', 'Vulture#9999'],
+    ['Walrus', 'src/img/deltas/walrus.jpg', '1993-12-30', 'https://steamcommunity.com/profiles/76561198002184444', '', 'Solfoyer#3261']
 ];
 
 $(document).ready(function () {
     fillLeaders();
     fillMembers();
+    new ClipboardJS(".clipboard");
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
 });
 
-function fillLeaders(){
+var timeout = false;
+
+function copyCp() {
+    if(timeout === false) {
+        $.notify({
+            // options
+            message: 'Copied to clipboard'
+        },{
+            // settings
+            type: 'dark',
+            delay: 2000,
+            allow_dismiss: false,
+            animate: {
+                enter: 'animated fadeInUp',
+                exit: 'animated fadeOutDown'
+            },
+            placement: {
+                from: "bottom",
+                align: "center"
+            }
+        });
+        timeout = true;
+    }
+
+    setTimeout(function(){
+        timeout = false;
+    }, 3000);
+}
+
+function calcAge(bdate) { // birthday is a date
+    birthday = new Date(bdate);
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function fillLeaders() {
     var html = '';
     var nbitems = 3;
 
-    for(var row = 0; row < leaders.length; row += nbitems)
-    {
-        html +='<div class="row text-center justify-content-center">\n'; // open div.row
-        for(var i = row; i < (row + nbitems); i++)
-        {
-            if(typeof members[i] !== "undefined") {
+    for (var row = 0; row < leaders.length; row += nbitems) {
+        html += '<div class="row text-center justify-content-center">\n'; // open div.row
+        for (var i = row; i < (row + nbitems); i++) {
+            if (typeof members[i] !== "undefined") {
                 html += '<div class="col-4">\n';
                 html += '<div class="card hovercard">\n';
                 html += '<div class="cardheader"></div>\n';
@@ -57,23 +98,34 @@ function fillLeaders(){
                 html += '<div class="info-title">\n';
                 html += leaders[i][0] + '\n';
                 html += '</div>\n';
+                ///////// AGE
                 if (leaders[i][2] === '') {
                     html += '<div class="info-desc"><br/></div>\n';
                 } else {
-                    html += '<div class="info-desc">' + leaders[i][2] + '</div>\n';
+                    var age = calcAge(leaders[i][2]);
+                    html += '<div class="age info-desc">' + age + ' ans</div>\n';
                 }
                 html += '<div class="info-desc">\n';
-                if (leaders[i][3] === '') {
+                ///////// VIDE
+                if(members[i][3] === '' && members[i][4] === '' && members[i][5] === ''){
                     html += '<div style="height: 34px"></div>\n';
-                } else {
+                }
+                ///////// STEAM
+                if (leaders[i][3] !== '') {
                     html += '<a href="' + leaders[i][3] + '">\n';
                     html += '<i class="fab fa-steam-symbol"></i>\n';
                     html += '</a>\n';
                 }
-
+                ///////// TWITCH
                 if (leaders[i][4] !== '') {
-                    html += '<a target="_blank" href="'+ leaders[i][4] +'">\n';
+                    html += '<a target="_blank" href="' + leaders[i][4] + '">\n';
                     html += '<i class="fab fa-twitch"></i>\n';
+                    html += '</a>\n';
+                }
+                ///////// DISCORD
+                if (leaders[i][5] !== '') {
+                    html += '<a target="_blank" class="discord clipboard" href="#" href="#" data-clipboard-text="' + leaders[i][5] + '" onclick="copyCp()">\n';
+                    html += '<i class="fab fa-discord" data-toggle="tooltip" data-placement="top" title="' + leaders[i][5] + '"></i>\n';
                     html += '</a>\n';
                 }
                 html += '</div>\n';
@@ -88,16 +140,14 @@ function fillLeaders(){
     $('#leaders').html(html);
 }
 
-function fillMembers(){
+function fillMembers() {
     var html = '';
     var nbitems = 3;
 
-    for(var row = 0; row < members.length; row += nbitems)
-    {
-        html +='<div class="row text-center justify-content-center">\n'; // open div.row
-        for(var i = row; i < (row + nbitems); i++)
-        {
-            if(typeof members[i] !== "undefined") {
+    for (var row = 0; row < members.length; row += nbitems) {
+        html += '<div class="row text-center justify-content-center">\n'; // open div.row
+        for (var i = row; i < (row + nbitems); i++) {
+            if (typeof members[i] !== "undefined") {
                 html += '<div class="col-4">\n';
                 html += '<div class="card hovercard">\n';
                 html += '<div class="cardheader"></div>\n';
@@ -108,23 +158,34 @@ function fillMembers(){
                 html += '<div class="info-title">\n';
                 html += members[i][0] + '\n';
                 html += '</div>\n';
+                ///////// AGE
                 if (members[i][2] === '') {
-                    html += '<div class="info-desc"><br/></div>\n';
+                    html += '<div class="age info-desc"><br/></div>\n';
                 } else {
-                    html += '<div class="info-desc">' + members[i][2] + '</div>\n';
+                    var age = calcAge(members[i][2]);
+                    html += '<div class="age info-desc">' + age + ' ans</div>\n';
                 }
                 html += '<div class="info-desc">\n';
-                if (members[i][3] === '') {
+                ///////// VIDE
+                if(members[i][3] === '' && members[i][4] === '' && members[i][5] === ''){
                     html += '<div style="height: 34px"></div>\n';
-                } else {
-                    html += '<a href="' + members[i][3] + '">\n';
+                }
+                ///////// STEAM
+                if (members[i][3] !== '') {
+                    html += '<a class="steam" href="' + members[i][3] + '">\n';
                     html += '<i class="fab fa-steam-symbol"></i>\n';
                     html += '</a>\n';
                 }
-
+                ///////// TWITCH
                 if (members[i][4] !== '') {
-                    html += '<a target="_blank" href="'+ members[i][4] +'">\n';
+                    html += '<a target="_blank" class="twitch" href="' + members[i][4] + '">\n';
                     html += '<i class="fab fa-twitch"></i>\n';
+                    html += '</a>\n';
+                }
+                ///////// DISCORD
+                if (members[i][5] !== '') {
+                    html += '<a target="_blank" class="discord clipboard" href="#" href="#" data-clipboard-text="' + members[i][5] + '" onclick="copyCp()">\n';
+                    html += '<i class="fab fa-discord" data-toggle="tooltip" data-placement="top" title="' + members[i][5] + '"></i>\n';
                     html += '</a>\n';
                 }
                 html += '</div>\n';
